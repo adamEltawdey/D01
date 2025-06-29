983bb47cb5008008ab6f76e26febb8718011887c
adama <adama@ADAM>
2025-06-29 13:34:47 +0300

    Add project files.

---
+++ First project/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your favotite number");
+        int yourNum = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("really  " + yourNum + "is my favorite number");
+
+    }
+}
